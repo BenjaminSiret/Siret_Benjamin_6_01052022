c7327ee95525e132735613f1e4263b707ee7c8f5
Benjamin Siret <benjaminsiret@outlook.fr>
2022-07-07 16:53:07 +0200

    minor change in html

--- scripts/utils/lightbox.js
+++ scripts/utils/lightbox.js
@@ -145,36 +145,3 @@ window.addEventListener("keydown", (e) => {
     }
   }
 });
-
-//************* A SUPPRIMER EN FIN DE PROJET **************
-
-// function displayLightbox() {
-//   const main = document.getElementById("main");
-//   const images = Array.from(document.querySelectorAll(".sample-image"));
-//   images.forEach((image) =>
-//     image.addEventListener("click", (e) => {
-//       e.preventDefault();
-//       const imageURL = image.getAttribute("src");
-//       let lightbox = document.createElement("div");
-//       lightbox.innerHTML = `<div id="lightbox">
-//         <button class="lightbox-cross">
-//           <img src="assets/icons/close-lightbox.svg" alt="Close dialog" />
-//           </button>
-//           <button class="lightbox-next">
-//           <img src="assets/icons/arrow.svg" alt="" />
-//           </button>
-//           <button class="lightbox-prev">
-//           <img src="assets/icons/arrow.svg" alt="" />
-//           </button>
-//           <div class="image-container">
-//           <img
-//           src="${imageURL}"
-//           alt=""
-//           class="image-lightbox"
-//           />
-//         </div>
-//         </div>`;
-//       main.appendChild(lightbox);
-//     })
-//   );
-// }
